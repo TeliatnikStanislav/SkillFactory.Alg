59bb0c638e71a051d84bcefc7e70fedb59fb5725
Stanislav <slava.telyatnik@mail.ru>
2023-12-10 12:25:40 +0100

    Добавьте файлы проекта.

---
+++ Alg/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Как вас зовут?";
+            Console.WriteLine(a);
+            string Name = Console.ReadLine();
+            Console.WriteLine($"Привет " + Name);
+            Console.WriteLine("Сколько элементов будет в массиве?");
+            int count = Convert.ToInt32(Console.ReadLine());
+            string[] array;
+            array = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Все элементы записаны");
+
+            Console.WriteLine("Введите свой возраст");
+            byte age = Convert.ToByte(Console.ReadLine());
+            if (age > 13) 
+            {
+                Console.WriteLine("Вы успешно зарегистрированы");
+            }
+            else
+                Console.WriteLine("Пользователи младше 14 лет не могут пройти регистрацию");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Alg/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Alg")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Alg")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a49db071-d375-4576-8092-3bf24a9271b5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
